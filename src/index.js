130b3de0c41e9213bd781d783cd3fd20083d3e41
Tomás Mellado <timellado@uc.cl>
2021-05-24 02:47:09 -0400

    Feat/spotify api (#11)
    * first commit, integration working
    
    * fix: file ordering
    
    * feat: login flow done
    
    * delete file
    
    * feat: next working, play half way
    
    * feat: play and pause working
    
    * feat: progress bar
    
    * feat: search and song reproduce

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js").then(registration => {
-        console.log("SW Registered");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW Registration Failed");
-        console.log(error);
-    })
+  navigator.serviceWorker.register("sw.js").then(registration => {
+    currentlyPlaying();
+    console.log("SW Registered");
+    console.log(registration);
+  }).catch(error => {
+    console.log("SW Registration Failed");
+    console.log(error);
+  })
 }
 
 function showNotification() {
-    const notification = new Notification("New message from Musicfy", {
-        body: "Discover new artists, albums and songs!"
-    })
+  const notification = new Notification("New message from Musicfy", {
+    body: "Discover new artists, albums and songs!"
+  })
 }
 
 if (Notification.permission !== "granted") {
-    Notification.requestPermission();
+  Notification.requestPermission();
 }
 
 document.getElementById("notify").onclick = showNotification;
 
 const mainDiv = document.getElementById('full-container');
-
-const apiUrl = 'https://api.spotify.com/v1/search';
 
 const switchTo = (view) => {
   const toRemove = mainDiv.querySelector('div');
@@ -45,22 +44,48 @@ document.getElementById('search-button').onclick = () => {
     document.getElementById('accept-search-button').onclick = () => {
       const searchBar = document.getElementById('search-bar');
       const toSearch = searchBar.value;
-      console.log(toSearch);
+      cleanSearch();
 
       // SEARCH SONGS ARTISTS AND ALBUMS HERE
+      let access_token = localStorage.getItem("access_token")
+      console.log(access_token);
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", SEARCH + `?q=${toSearch}&type=track%2Cartist%2Calbum&market=US&limit=10&offset=5`, true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+      xhr.onreadystatechange = function (aEvt) {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            let data = JSON.parse(xhr.responseText);
+            console.log(data);
+            data.artists.items.forEach(e => {
+              showResultArtists(e)
+            });
+            data.albums.items.forEach(e => {
+              showResultAlbums(e)
+            });
+            data.tracks.items.forEach(e => {
+              showResultSongs(e)
+            });
+          }
+          else {
+            dump("Error loading page\n");
+          }
+        }
+      };
+      xhr.send(null);
     };
   }, 200);
 };
 
 
-// document.getElementById('home-button').onclick = () => {
-//   console.log('SWITCH TO HOME!');
-//   switchTo('home');
-// };
+document.getElementById('home-button').onclick = () => {
+  console.log('SWITCH TO HOME!');
+  switchTo('home');
+};
 
-// window.addEventListener('load', () => {
-//   currentlyPlaying();
-//   // document.getElementById('my-button').onclick = () => {
-//   //   document.querySelector('content-display').setAttribute('title', 'Honorary Astronaut');
-//   // };
-// });
+// if (navigator.serviceWorker) {
+//   window.addEventListener('load', () => {
+//     currentlyPlaying();
+//   });
+// }
