64b3a4fa644fb702de14a22d6978bbd5df5ba178
Noemi Crosby <nicrosby@uc.cl>
2021-05-23 22:04:51 -0400

    notify + sw + cache

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-const test = 'This is a test var';
-console.log(test);
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("SW Registered");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW Registration Failed");
+        console.log(error);
+    })
+}
+
+function showNotification() {
+    const notification = new Notification("New message from Spoofy", {
+        body: "Discover new artists, albums and songs!"
+    })
+}
+
+if (Notification.permission !== "granted") {
+    Notification.requestPermission();
+}
+
+document.getElementById("notify").onclick = showNotification;
